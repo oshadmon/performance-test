195b1a3d81cb8fd5f69c05fec8648f3932c43d52
Ori Shadmon <oshadmon@gmail.com>
2025-07-09 15:53:59 -0700

    in progress
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import re
 import time
 import json
 import urllib3
-import math
+# import math
 
-from concurrent.futures import ThreadPoolExecutor, as_completed
+# from concurrent.futures import ThreadPoolExecutor, as_completed
 from typing import Union, List, Tuple
-
+from collections   import defaultdict
 
 CONNS = {}
 
@@ -56,7 +56,7 @@ def generate_row(columns: list) -> dict:
     }
 
 
-def insert_data(dbms: str, table: str, payload: list):
+def insert_data(dbms: str, table: str, payload:(list or dict)):
     global CONNS
     headers = {
         'type': 'json',
@@ -73,7 +73,12 @@ def insert_data(dbms: str, table: str, payload: list):
         else:
             conn = None
     try:
-        response = urllib3.request(method='PUT', url=f'http://{conn}', headers=headers, body=json.dumps(payload))
+        if isinstance(payload, dict):
+            for table in payload:
+                headers['table'] = table
+                response = urllib3.request(method='PUT', url=f'http://{conn}', headers=headers, body=json.dumps(payload[table]))
+        else:
+            response = urllib3.request(method='PUT', url=f'http://{conn}', headers=headers, body=json.dumps(payload))
     except Exception as error:
         raise Exception(f'❌ Failed insert to {conn}: {error}')
     else:
@@ -91,10 +96,11 @@ def main():
     parser.add_argument('--batch-size', type=int, default=10, help='Rows per insert (0 = auto)')
     parser.add_argument('--size', type=str, default="10MB", help='Target table size (e.g. 10MB, 1GB) (0 = ignore)')
     parser.add_argument('--hz', type=int, default=0, help='Inserts per second (0 = no rate limit)')
-    parser.add_argument('--threads', type=int, default=1, help='Number of threads to run in parallel')
+    # parser.add_argument('--threads', type=int, default=1, help='Number of threads to run in parallel')
     parser.add_argument('--run-time', type=float, default=0, help='Run for X seconds (0 = ignore)')
     parser.add_argument('--dbms', type=str, default='test', help='Database name')
     parser.add_argument('--table', type=str, default='rand_data', help='Table name')
+    parser.add_argument('--column-as-table', type=bool, nargs='?', const=True, default=False, help='convert columns into timestamp/value tables')
     args = parser.parse_args()
 
     for conn in args.conn.split(','):
@@ -106,44 +112,45 @@ def main():
         else ([f'column_{i+1}' for i in range(args.num_columns)], float('inf'))
     )
 
-    args.batch_size = math.ceil(total_rows / args.threads) if args.batch_size < 1 else args.batch_size
+    # args.batch_size = 1000 if args.batch_size < 1 else args.batch_size
     sleep_time = args.batch_size / args.hz if args.hz > 0 else 0
 
-    print(f"🎯 Target row count: {total_rows if total_rows != float('inf') else '∞'} | Batch size: {args.batch_size:,} | Threads: {args.threads} | Sleep: {sleep_time:.2f}s")
+    print(f"🎯 Target row count: {total_rows if total_rows != float('inf') else '∞'} | Batch size: {args.batch_size:,} |  Sleep: {sleep_time:.2f}s")
 
     start_time = time.time()
     total_inserted = 0
 
-    with ThreadPoolExecutor(max_workers=args.threads) as executor:
-        futures = []
 
-        while True:
-            if 0 < args.run_time <= time.time() - start_time:
-                break
+    while True:
+        if 0 < args.run_time <= time.time() - start_time:
+            break
 
-            if args.run_time == 0 and total_inserted >= total_rows:
-                break
+        if args.run_time == 0 and total_inserted >= total_rows:
+            break
 
-            batch_size = min(args.batch_size, total_rows - total_inserted) if total_rows != float('inf') else args.batch_size
-            batch = [generate_row(columns) for _ in range(batch_size)]
+        batch_size = min(args.batch_size, total_rows - total_inserted) if total_rows != float('inf') else args.batch_size
+        batch = [generate_row(columns) for _ in range(batch_size)]
+        if args.column_as_table:
+            data = defaultdict(list)
+            for row in batch:
+                timestamp = row['timestamp']
+                for key, value in row.items():
+                    if key == 'timestamp':
+                        continue
+                    data[f"{args.table}_{key}"].append({'timestamp': timestamp, 'value': value})
+            batch = data
 
-            # Submit insert job to executor
-            future = executor.submit(insert_data, args.dbms, args.table, batch)
-            futures.append(future)
-            total_inserted += batch_size
+        # Submit insert job to executor
+        insert_data(args.dbms, args.table, batch)
 
-            if sleep_time:
-                time.sleep(sleep_time)
+        total_inserted += batch_size
 
-        # Wait for all tasks to complete
-        for future in as_completed(futures):
-            try:
-                future.result()
-            except Exception as e:
-                print(f"❌ Thread error: {e}")
+        if sleep_time:
+            time.sleep(sleep_time)
 
     elapsed = round(time.time() - start_time, 2)
     print(f"✅ Done. Inserted {total_inserted:,} rows in {elapsed} seconds")
+
 
 if __name__ == "__main__":
     main()
